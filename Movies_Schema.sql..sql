d9e1f3cd86ad2855ad9e598f3c816b1e9edb5e12
Marquis Green <133602805+Reaper77777@users.noreply.github.com>
2026-07-27 18:12:58 -0500

    Add files via upload

---
+++ Movies_Schema.sql..sql
@@ -0,0 +1,63 @@
+﻿Cloning into 'C:\Users\reape\source\repos\Reaper77777\Movies-Database'...
+POST git-upload-pack (193 bytes)
+POST git-upload-pack (220 bytes)
+Remote: Enumerating objects: 4, done.        
+Remote: Counting objects:  25% (1/4)        
+Remote: Counting objects:  50% (2/4)        
+Remote: Counting objects:  75% (3/4)        
+Remote: Counting objects: 100% (4/4)        
+Remote: Counting objects: 100% (4/4), done.        
+Remote: Compressing objects:  25% (1/4)        
+Remote: Compressing objects:  50% (2/4)        
+Remote: Compressing objects:  75% (3/4)        
+Remote: Compressing objects: 100% (4/4)        
+Remote: Compressing objects: 100% (4/4), done.        
+Remote: Total 4 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)        
+Cloning into 'C:\Users\reape\source\repos\Movies-Database'...
+POST git-upload-pack (193 bytes)
+POST git-upload-pack (220 bytes)
+Remote: Enumerating objects: 4, done.        
+Remote: Counting objects:  25% (1/4)        
+Remote: Counting objects:  50% (2/4)        
+Remote: Counting objects:  75% (3/4)        
+Remote: Counting objects: 100% (4/4)        
+Remote: Counting objects: 100% (4/4), done.        
+Remote: Compressing objects:  25% (1/4)        
+Remote: Compressing objects:  50% (2/4)        
+Remote: Compressing objects:  75% (3/4)        
+Remote: Compressing objects: 100% (4/4)        
+Remote: Compressing objects: 100% (4/4), done.        
+Remote: Total 4 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)        
+Opening repositories:
+C:\Users\reape\source\repos\Reaper77777\Movies-Database
+Root element is missing.
+Opening repositories:
+C:\Users\reape\source\repos\Movies-Database
+Root element is missing.
+Opening repositories:
+C:\Users\reape\source\repos\Reaper77777\Movies-Database
+Root element is missing.
+Opening repositories:
+C:\Users\reape\source\repos\Movies-Database
+Root element is missing.
+Cloning into 'C:\Users\reape\Documents\SQL Server Management Studio'...
+POST git-upload-pack (193 bytes)
+POST git-upload-pack (220 bytes)
+Remote: Enumerating objects: 4, done.        
+Remote: Counting objects:  25% (1/4)        
+Remote: Counting objects:  50% (2/4)        
+Remote: Counting objects:  75% (3/4)        
+Remote: Counting objects: 100% (4/4)        
+Remote: Counting objects: 100% (4/4), done.        
+Remote: Compressing objects:  25% (1/4)        
+Remote: Compressing objects:  50% (2/4)        
+Remote: Compressing objects:  75% (3/4)        
+Remote: Compressing objects: 100% (4/4)        
+Remote: Compressing objects: 100% (4/4), done.        
+Remote: Total 4 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)        
+Opening repositories:
+C:\Users\reape\Documents\SQL Server Management Studio
+Root element is missing.
+Opening repositories:
+C:\Users\reape\source\repos\Reaper77777\Movies-Database
+Root element is missing.
