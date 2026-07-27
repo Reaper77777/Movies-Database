5c8902fb9fdbd576c3bf37a091bae3c5151077e6
Marquis Green <133602805+Reaper77777@users.noreply.github.com>
2026-07-27 18:14:13 -0500

    Delete SQLQuery4.sql

--- SQLQuery4.sql
+++
@@ -1,9 +0,0 @@
--- Forcefully disconnect everyone from the Movies database and drop it
-USE master;
-GO
-
-ALTER DATABASE Movies SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-GO
-
-DROP DATABASE Movies;
-GO
